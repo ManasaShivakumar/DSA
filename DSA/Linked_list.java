9c55da16dafb1e750cf8d0b2b616fc74858e867c
Manasa B S <manasabs065@gmail.com>
2026-08-03 15:48:06 +0530

    modified

--- DSA/Linked_list.java
+++ DSA/Linked_list.java
@@ -1,4 +1,3 @@
-import org.w3c.dom.Node;
 
 public class Linked_list {
     public static class Node{
@@ -286,17 +285,100 @@ public class Linked_list {
         //remove cycle
         prev.next = null;
     }
+
+    private Node getMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    private Node merge(Node head1, Node head2){
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+        while(head1 != null && head2 != null){
+            if(head1.data <= head2.data){
+                temp.next = head1;
+                head1 = head1.next;  
+                temp = temp.next;              
+            }
+            else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }           
+        }
+        while(head1 != null){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while(head2 != null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+
+    public Node mergeSort(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node mid = getMid(head);
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node leftHalf = mergeSort(head);
+        Node rightHalf = mergeSort(rightHead);
+
+        return merge(leftHalf, rightHalf); 
+    }
+    public void zig_ZagLL(){
+        //find Mid
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+        //Reverse 2nd half
+        Node prev = null;
+        Node curr = mid.next;
+        Node next;
+        mid.next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        //Alternate merging
+        Node RH = prev;
+        Node LH = head;
+        Node nextL, nextR;
+        while(LH != null && RH != null){
+            nextL = LH.next;
+            LH.next = RH;
+            nextR = RH.next;
+            RH.next = nextL;
+
+            LH = nextL;
+            RH = nextR;
+        }                
+    }
     public static void main(String[] args) {
-        //Linked_list ll = new Linked_list();        
-        
-        head = new Node(1);
-        Node temp = new Node(2);
-        head.next = temp;
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
-        head.next.next.next.next =temp;       
-        System.out.println(isCycle());     
-        removeCycle();
-        System.out.println(isCycle());        
+        Linked_list ll = new Linked_list();        
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
+        ll.print();
+        ll.zig_ZagLL();
+        ll.print();             
     }    
 }
