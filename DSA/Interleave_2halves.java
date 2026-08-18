9c23de36c9d5ebd3da8f61c37d4a0a1889a1c0d9
Manasa B S <manasabs065@gmail.com>
2026-08-18 18:27:20 +0530

    new data structures

---
+++ DSA/Interleave_2halves.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Interleave_2halves {
+    public static void interLeave(Queue<Integer> q){
+        int size = q.size();
+        Queue<Integer> first_q = new LinkedList<>();
+        for(int i=0; i<size/2; i++){
+            first_q.add(q.remove());
+        }
+        while(!first_q.isEmpty()){
+            q.add(first_q.remove());
+            q.add(q.remove());
+        }
+    }
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+        interLeave(q);
+        while(!q.isEmpty()){
+            System.out.print(q.remove()+" ");            
+        }
+    }    
+}
